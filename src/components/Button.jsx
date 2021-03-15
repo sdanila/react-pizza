5f069de60ac6d7af88ebb3f63e7d3fab48297a61
Danila <sdanila512@gmail.com>
2021-03-15 16:11:47 +0500

    added components(Header, Button, Categories, Sort)

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const Button = ({ className, outline, children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={classNames('button', className, {
+        'button--outline': outline,
+      })}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Categories({ items }) {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+  return (
+    <div className="categories">
+      <ul>
+        <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
+          Все
+        </li>
+        {items &&
+          items.map((item, index) => (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${item}_${index}`}>
+              {item}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
